1af8d0bce6390bfcc0ea44c14debc95fba51accb
gvanhoy <basebzombie@gmail.com>
2017-10-13 12:29:00 -0700

    Added file source

---
+++ include/pureliquid/file_source.h
@@ -0,0 +1,56 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2017 <+YOU OR YOUR COMPANY+>.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+
+#ifndef INCLUDED_PURELIQUID_FILE_SOURCE_H
+#define INCLUDED_PURELIQUID_FILE_SOURCE_H
+
+#include <pureliquid/api.h>
+#include <gnuradio/block.h>
+
+namespace gr {
+  namespace pureliquid {
+
+    /*!
+     * \brief <+description of block+>
+     * \ingroup pureliquid
+     *
+     */
+    class PURELIQUID_API file_source : virtual public gr::block
+    {
+     public:
+      typedef boost::shared_ptr<file_source> sptr;
+
+      /*!
+       * \brief Return a shared_ptr to a new instance of pureliquid::file_source.
+       *
+       * To avoid accidental use of raw pointers, pureliquid::file_source's
+       * constructor is in a private implementation
+       * class. pureliquid::file_source::make is the public interface for
+       * creating new instances.
+       */
+      static sptr make(std::string fileName, bool repeat, unsigned int period_ms);
+    };
+
+  } // namespace pureliquid
+} // namespace gr
+
+#endif /* INCLUDED_PURELIQUID_FILE_SOURCE_H */
+
