2f7022ff4fd8949cf708c3bb84a1a5dcaeb82a43
gvanhoy <basebzombie@gmail.com>
2017-10-14 10:13:42 -0700

    Added flex_tx

--- lib/flex_tx_impl.h
+++ lib/flex_tx_impl.h
@@ -1,17 +1,17 @@
 /* -*- c++ -*- */
-/*
+/* 
  * Copyright 2017 <+YOU OR YOUR COMPANY+>.
- *
+ * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- *
+ * 
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -22,7 +22,6 @@
 #define INCLUDED_PURELIQUID_FLEX_TX_IMPL_H
 
 #include <pureliquid/flex_tx.h>
-#include <liquid/liquid.h>
 
 namespace gr {
   namespace pureliquid {
@@ -30,24 +29,11 @@ namespace gr {
     class flex_tx_impl : public flex_tx
     {
      private:
-         unsigned int           d_modulation;
-         unsigned int           d_inner_code;
-         unsigned int           d_outer_code;
-         unsigned char *        d_header;
-         unsigned char *        d_payload;
-         flexframegenprops_s    d_fgprops;
-         flexframegen           d_fg;
-         unsigned long int      d_num_frames;
+      // Nothing to declare in this block.
 
      public:
       flex_tx_impl(unsigned int modulation, unsigned int inner_code, unsigned int outer_code);
       ~flex_tx_impl();
-      void send_pkt(pmt::pmt_t pdu);
-      void configure(pmt::pmt_t configuration);
-
-      void set_modulation(unsigned int modulation);
-      void set_inner_code(unsigned int inner_code);
-      void set_outer_code(unsigned int outer_code);
 
       // Where all the action really happens
       int work(int noutput_items,
