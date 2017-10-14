a57715f6422c93cbadb123454d6b2a4b9f4c3d6b
gvanhoy <basebzombie@gmail.com>
2017-10-14 10:22:35 -0700

    Maybe fixed

--- include/pureliquid/flex_tx.h
+++ include/pureliquid/flex_tx.h
@@ -1,17 +1,17 @@
 /* -*- c++ -*- */
-/* 
+/*
  * Copyright 2017 <+YOU OR YOUR COMPANY+>.
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -47,6 +47,9 @@ namespace gr {
        * creating new instances.
        */
       static sptr make(unsigned int modulation, unsigned int inner_code, unsigned int outer_code);
+      virtual void set_modulation(unsigned int modulation) = 0;
+      virtual void set_inner_code(unsigned int inner_code) = 0;
+      virtual void set_outer_code(unsigned int outer_code) = 0;
     };
 
   } // namespace pureliquid
